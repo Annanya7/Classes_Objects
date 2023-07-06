958d68e06283188383598fd6df7ae312b0371362
Annanya7 <78608477+Annanya7@users.noreply.github.com>
2023-07-07 00:52:22 +0530

    Add files via upload

---
+++ teach.py
@@ -0,0 +1,7 @@
+from teacher import Teacher
+t=Teacher()
+t.set_id(101)
+print("The id=",t.get_id())
+t.set_name('Noah')
+print("The name=",t.get_name())
+
---
+++ teacher.py
@@ -0,0 +1,15 @@
+class Teacher:
+    def set_id(self,id):
+        self.id=id
+    def get_id(self):
+        return self.id
+    def set_name(self,name):
+        self.name=name
+    def get_name(self):
+        return self.name
+    def set_salary(self,sal):
+        self.sal=sal
+    def get_salary(self):
+        return self.sal
+
+    
